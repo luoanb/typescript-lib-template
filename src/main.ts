ba9fefcd068052d554b773bc63082d006992ae4e
LuoAnbing <luoanb@163.com>
2023-03-27 10:09:53 +0800

    更新

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,22 @@
 // import "core-js"
 import axios from "axios";
 import { xml2json } from "xml-js";
-var xml =
-  '<?xml version="1.0" encoding="utf-8"?>' +
-  '<note importance="high" logged="true">' +
-  "    <title>Happy</title>" +
-  "    <todo>Work</todo>" +
-  "    <todo>Play</todo>" +
-  "</note>";
+var xml = '<?xml version="1.0" encoding="utf-8"?>' + '<note importance="high" logged="true">' + "    <title>Happy</title>" + "    <todo>Work</todo>" + "    <todo>Play</todo>" + "</note>";
 var result1 = xml2json(xml, { compact: true, spaces: 4 });
 var result2 = xml2json(xml, { compact: false, spaces: 4 });
 console.log(result1, "\n", result2);
-const axiostest = axios.get("www.baidu.com").then((res) => {
-  console.log(res);
-});
+const axiostest = axios
+  .get("www.baidu.com")
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// const axiostest1 = async () => {
+//   return axios.get("www.baidu.com").then((res) => {
+//     console.log(res);
+//   });
+// };
 export { result1, result2, axiostest };
